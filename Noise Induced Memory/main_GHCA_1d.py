597d2baf7662eecdfe8e9591179ce434179e40fa
Yulin Chen <56228115+Yulin832@users.noreply.github.com>
2020-11-24 11:09:03 +0000

    Update main_GHCA_1d.py

--- Noise Induced Memory/main_GHCA_1d.py
+++ Noise Induced Memory/main_GHCA_1d.py
@@ -1,8 +1,22 @@
-import GHCA_1d
+import GHCA_1d 
+import line_profiler
 
+import line_profiler 
+import atexit
+
+import sys, trace
+
+profile = line_profiler.LineProfiler()
+atexit.register(profile.print_stats)
 
 lattice = GHCA_1d.GHCA()
-lattice.run()
+tracer = trace.Trace( trace=0, count=0, timing=True,  countcallers=True)
+tracer.run('lattice.run()') # init your class and track the function calls
+tracer.results().write_results(show_missing=False) # print result to the screen
+
+
+
+
 
 
 
